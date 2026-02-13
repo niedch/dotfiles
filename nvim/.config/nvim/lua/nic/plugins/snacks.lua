e8fc289fe9bf44d6ae9023a9aabb4e3f5c708d8b
nic <christoph.niederer99@gmail.com>
2026-02-13 18:06:33 +0100

    fix: Key binds

--- nvim/.config/nvim/lua/nic/plugins/snacks.lua
+++ nvim/.config/nvim/lua/nic/plugins/snacks.lua
@@ -91,24 +91,6 @@ return {
     { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
     { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
     { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
-    {
-      "<leader>N",
-      desc = "Neovim News",
-      function()
-        Snacks.win({
-          file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-          width = 0.6,
-          height = 0.6,
-          wo = {
-            spell = false,
-            wrap = false,
-            signcolumn = "yes",
-            statuscolumn = " ",
-            conceallevel = 3,
-          },
-        })
-      end,
-    }
   },
     init = function()
       local function get_visual_selection()
