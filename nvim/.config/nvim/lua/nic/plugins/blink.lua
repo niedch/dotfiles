c294bdf3fa5300fa90bfaae2753ed30cc4aa3e63
nic <christoph.niederer99@gmail.com>
2026-05-19 07:11:25 +0200

    fix(cleanup): Cleanup

--- nvim/.config/nvim/lua/nic/plugins/blink.lua
+++ nvim/.config/nvim/lua/nic/plugins/blink.lua
@@ -27,7 +27,7 @@ return {
 				use_nvim_cmp_as_default = false,
 				-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
 				-- Adjusts spacing to ensure icons are aligned
-				nerd_font_variant = "normal",
+				nerd_font_variant = "mono",
 			},
 			signature = { enabled = true },
 			completion = {
--- nvim/.config/nvim/lua/nic/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/nic/plugins/lsp/lspconfig.lua
@@ -35,18 +35,18 @@ return {
 				virtual_text = true,
 			})
 
-			vim.api.nvim_create_autocmd("LspAttach", {
-				desc = "Enable inlay hints",
-				callback = function(event)
-					local id = vim.tbl_get(event, "data", "client_id")
-					local client = id and vim.lsp.get_client_by_id(id)
-					if client == nil or not client.supports_method("textDocument/inlayHint") then
-						return
-					end
-
-					vim.lsp.inlay_hint.enable(true, { bufnr = event.buf })
-				end,
-			})
+			-- vim.api.nvim_create_autocmd("LspAttach", {
+			-- 	desc = "Enable inlay hints",
+			-- 	callback = function(event)
+			-- 		local id = vim.tbl_get(event, "data", "client_id")
+			-- 		local client = id and vim.lsp.get_client_by_id(id)
+			-- 		if client == nil or not client.supports_method("textDocument/inlayHint") then
+			-- 			return
+			-- 		end
+			--
+			-- 		vim.lsp.inlay_hint.enable(true, { bufnr = event.buf })
+			-- 	end,
+			-- })
 
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
